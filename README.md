db152921b4e5b0b13ab7da04f5558055e9f95f4d
Jason Watmore <jason.watmore@hotmail.com>
2018-06-25 18:33:05 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# angular-6-communicating-between-components
+
+Angular 6 - Communicating Between Components with Observable & Subject
+
+To see a demo and further details go to http://jasonwatmore.com/post/2018/06/25/angular-6-communicating-between-components-with-observable-subject
---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { MessageService } from './_services/index';
+
+@Component({
+    selector: 'app',
+    templateUrl: 'app.component.html'
+})
+
+export class AppComponent implements OnDestroy {
+    message: any;
+    subscription: Subscription;
+
+    constructor(private messageService: MessageService) {
+        // subscribe to home component messages
+        this.subscription = this.messageService.getMessage().subscribe(message => { this.message = message; });
+    }
+
+    ngOnDestroy() {
+        // unsubscribe to ensure no memory leaks
+        this.subscription.unsubscribe();
+    }
+}
